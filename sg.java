de41a30116a3c3386d32dd2f558106dad058e7e0
hemanth1624 <hemanthsg30@gmail.com>
2025-03-12 10:24:34 +0530

    Update sg.java

--- sg.java
+++ sg.java
@@ -2,9 +2,9 @@ class hemanth
 {
 	public static void main(String[]args)
 	{
-		System.out.println("hello world")
-		System.out.println("welcome to spt")
-		System.out.println("computer science department")
+		System.out.println("hello world");
+		System.out.println("welcome to spt");
+		System.out.println("computer science department");
 	}       
 }
 
